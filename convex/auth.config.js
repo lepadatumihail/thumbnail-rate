f2c4122bd0528423f5e360f54850068f9af1dfd4
Mihail <lepadatu.mihail1@gmail.com>
2024-01-29 23:31:35 +0100

    feat: Setup next, clerk, convex

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://happy-turkey-97.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { SignInButton, SignOutButton, auth, useSession } from '@clerk/nextjs';
+import { useMutation, useQuery } from 'convex/react';
+import Image from 'next/image';
+import { api } from '../../convex/_generated/api';
+
+export default function Home() {
+  const { isSignedIn } = useSession();
+  const createThumbnail = useMutation(api.thumbnail.createThumbnail);
+  const thumbnails = useQuery(api.thumbnail.getThumbnailsForUser);
+  console.log(thumbnails);
+
+  return (
+    <main className=''>
+      {!isSignedIn ? <SignInButton /> : <SignOutButton />}
+
+      {isSignedIn && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            const title = formData.get('title') as string;
+            createThumbnail({ title });
+            e.currentTarget.reset();
+          }}
+        >
+          <label>Title</label>
+          <input name='title' className='text-black' />
+          <button type='submit'>create</button>
+        </form>
+      )}
+      {thumbnails?.map((thumb) => (
+        <div key={thumb._id}>{thumb.title}</div>
+      ))}
+    </main>
+  );
+}
